5cbb9f62e1063e20ba55199522872c4a36560784
Merve AYDIN <maydin@gayebilisim.com>
2021-12-15 16:20:19 +0300

    🎉

---
+++ WebAPI/Controllers/DCandidateController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DCandidateController : ControllerBase
+    {
+        private readonly DonationDBContext _context;
+
+        public DCandidateController(DonationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("GetAll")]
+        public async Task<ActionResult<IEnumerable<DCandidate>>> GetAllDCandidates()
+        {
+            return await _context.DCandidates.ToListAsync();
+        }
+
+
+        [HttpPost("GetById")]
+        public async Task<ActionResult<DCandidate>> GetByIdDCandidate(int id)
+        {
+            var dCandidate = await _context.DCandidates.FindAsync(id);
+
+            if (dCandidate == null)
+            {
+                return NotFound();
+            }
+
+            return dCandidate;
+        }
+
+
+        [HttpPost("Update")]
+        public async Task<IActionResult> UpdateDCandidate(int id, DCandidate dCandidate)
+        {
+            dCandidate.Id = id;
+
+            _context.Entry(dCandidate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DCandidateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpPost("Add")]
+        public async Task<ActionResult<DCandidate>> AddDCandidate(DCandidate dCandidate)
+        {
+            _context.DCandidates.Add(dCandidate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetByIdDCandidate", new { id = dCandidate.Id }, dCandidate);
+        }
+
+
+        [HttpPost("Delete")]
+        public async Task<ActionResult<DCandidate>> DeleteDCandidate(int id)
+        {
+            var dCandidate = await _context.DCandidates.FindAsync(id);
+            if (dCandidate == null)
+            {
+                return NotFound();
+            }
+
+            _context.DCandidates.Remove(dCandidate);
+            await _context.SaveChangesAsync();
+
+            return dCandidate;
+        }
+
+        private bool DCandidateExists(int id)
+        {
+            return _context.DCandidates.Any(e => e.Id == id);
+        }
+    }
+}
